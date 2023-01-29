1fcbf62197fcfb8e7f3c8c966749728d61dd22f4
Jonathan Plaras <jonplaras@gmail.com>
2023-01-29 15:17:48 -0500

    removed comments from schema. adjusted Tag and Category model for seeding, it now works.

--- models/Tag.js
+++ models/Tag.js
@@ -9,12 +9,13 @@ Tag.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     tag_name: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true
     }
   },
   {
